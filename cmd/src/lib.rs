f4318494961fb09966342cbc1295ffd8d9c953af
ponurakk <p0unrakg@int.pl>
2022-11-12 11:36:45 +0100

    Changed from clap derive to command builder

--- cmd/src/lib.rs
+++ cmd/src/lib.rs
@@ -1,29 +1,7 @@
-use clap::Parser;
+use clap::{ arg, command, value_parser };
+use std::path::PathBuf;
 use colored::Colorize;
 
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Don't run the web view
-    #[arg(short, long)]
-    stop_web: bool,
-
-    /// Address on which server should run
-    #[arg(short, long, default_value = "127.0.0.1")]
-    address: String,
-
-    /// Port for the server
-    #[arg(short, long, default_value = "3000")]
-    port: String,
-
-    /// Show api logs
-    #[arg(short, long)]
-    log: bool,
-
-    /// Main folder
-    #[arg(short, long, default_value = "files")]
-    dir: String,
-}
 
 pub fn main() {
     println!("{}", r"
@@ -32,15 +10,66 @@ pub fn main() {
  / /__ / / / __// __/ / / / -_)     / /__   / // / /    /
 /____//_/  \__/ \__/ /_/  \__/      \___/  /____/ /_/|_/
     ".red().bold());
-    let args: Args = Args::parse();
 
-    // println!("{:#?}", &args);
+    let matches = command!()
+        .arg(arg!(
+            -s --"stop-web" "Don't run the web view"
+        ))
+        .arg(arg!(
+                -a --address <ADDRESS> "Address on which server should run"
+            )
+            .default_value("127.0.0.1")
+        )
+        .arg(arg!(
+                -p --port <PORT> "Port for the server"
+            )
+            .default_value("3000")
+        )
+        .arg(arg!(
+            -l --log "Show api logs"
+        ))
+        .arg(arg!(
+                -d --dir <DIR> "Main folder for storing files"
+            )
+            .value_parser(value_parser!(PathBuf))
+            .default_value("files")
+        )
+        .arg(arg!(
+                --db <DATABASE> "Database folder name"
+            )
+            .value_parser(value_parser!(PathBuf))
+            .default_value("db")
+        )
+        .arg(arg!(
+            --"clear-database" "Clear the database"
+        ))
+        .arg(arg!(
+            --"disable-login" --"d-l" "No one will be allowed to create new account"
+        ))
+        .get_matches();
 
-    if !args.stop_web {
-        let server = cdn_server_api::main(args.address, args.port.parse().unwrap(), args.log, args.dir);
-        match server {
-            Ok(v) => v,
-            Err(e) => panic!("{}", e),
-        }
+    let stop_web = matches.get_one::<bool>("stop-web").unwrap().clone();
+    let address = matches.get_one::<String>("address").unwrap().clone();
+    let port = matches.get_one::<String>("port").unwrap().clone().parse::<u16>().unwrap();
+    let log= matches.get_one::<bool>("log").unwrap().clone();
+    let dir = matches.get_one::<PathBuf>("dir").unwrap().clone();
+    let db = matches.get_one::<PathBuf>("db").unwrap().clone();
+    let disable_login = matches.get_one::<bool>("disable-login").unwrap().clone();
+    let clear_database = matches.get_one::<bool>("clear-database").unwrap().clone();
+
+    let server = little_cdn_api::main(
+        stop_web,
+        address,
+        port,
+        log,
+        dir,
+        db,
+        clear_database,
+        disable_login
+    );
+
+    match server {
+        Ok(v) => v,
+        Err(e) => panic!("{}", e),
     }
 }
